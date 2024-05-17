d319bad9ffb0d22a55efd3699809b600e6eefe56
Loup Lobet <loup.lobet@pi.esisar.grenoble-inp.fr>
2024-05-17 12:54:52 +0000

    initial commit

---
+++ util.c
@@ -0,0 +1,78 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <limits.h>
+
+#include "util.h"
+
+char *progname = "";
+
+void
+sysfatal(const char *fmt, ...)
+{
+	va_list ap;
+
+	va_start(ap, fmt);
+	fprintf(stderr, "%s: ", progname);
+	vfprintf(stderr, fmt, ap);
+	va_end(ap);
+	fputc('\n', stderr);
+	exit(1);
+}
+
+void
+syslog(const char *fmt, ...)
+{
+	va_list ap;
+
+	va_start(ap, fmt);
+	fprintf(stderr, "%s: ", progname);
+	vfprintf(stderr, fmt, ap);
+	va_end(ap);
+	fputc('\n', stderr);
+}
+
+void *
+emalloc(uint n)
+{
+	void *p = NULL;
+
+	p = malloc(n);
+	if (p == NULL)
+		error("malloc");
+	memset(p, 0, n);
+	return p;
+}
+
+void *
+erealloc(void *q, uint n)
+{
+	void *p = NULL;
+
+	p = realloc(q, n);
+	if (p == NULL)
+		error("realloc");
+	return p;
+}
+
+long
+estrtol(const char *nptr, int base)
+{
+	char *endptr = NULL;
+	errno = 0;
+	long n;
+
+	n = strtol(nptr, &endptr, base);
+	if ((endptr == nptr) || ((n == LONG_MAX || n == LONG_MIN)
+	   && errno == ERANGE))
+		error("invalid integer: %s", nptr);
+	return n;
+}
+
+void
+setprogname(char **s, char *argv0)
+{
+	*s = argv0;
+}
---
+++ util.h
@@ -0,0 +1,62 @@
+#ifndef _UTIL_H_
+#define _UTIL_H_
+
+#include <string.h>
+#include <stdio.h>
+
+#define MIN(A, B) (((A) > (B)) ? (B) : (A))
+#define MAX(A, B) (((A) < (B)) ? (B) : (A))
+#define LEN(A) (sizeof(A)/sizeof(*A))
+
+#define nil (void *)0
+
+#define ARG (argc > 1 ? (argc--, *(++argv)): nil)
+#define OPTS(body) \
+	setprogname(&progname, *argv);\
+	argc--;\
+	argv++;\
+	while (argc > 0) {\
+		if (**argv != '-')\
+			break;\
+		else {\
+			for (char *opts = *argv + 1; *opts; opts++) {\
+				switch (*opts) {\
+				body\
+				}\
+			}\
+			argc--;\
+			argv++;\
+		}\
+	}
+
+typedef long long vlong;
+
+typedef signed char int8;
+typedef signed short int16;
+typedef signed int int32;
+typedef signed long long int64;
+
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned int uint32;
+typedef unsigned long long uint64;
+
+typedef unsigned char uchar;
+typedef unsigned short ushort;
+typedef unsigned int uint;
+typedef unsigned long ulong;
+typedef unsigned long long uvlong;
+
+typedef void **allocstack;
+
+void	 sysfatal(const char *, ...);
+void	 syslog(const char *, ...);
+void	*emalloc(uint);
+void	*erealloc(void *, uint);
+long 	 estrtol(const char *nptr, int base);
+
+void setprogname(char **, char *);
+
+extern char *progname;
+
+#endif
